cdbb45fe48a9e69bbcae942ce76927e382983a0e
Rufus Sai <tsj20160326@gmail.com>
2021-01-29 18:42:41 +0000

    Create init-worlddist artisan command to replace execution of controller

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\InitWorldDist;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('init:worlddist', function () {
+    $initWorldDist = new InitWorldDist;
+    $initWorldDist->handle();
+})->describe('initial the record in world_dist table.');
